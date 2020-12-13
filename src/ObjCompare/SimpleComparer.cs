6534c52eb05cd0a9999ed37f4a18cb562bd7db15
Fernando Gonçalves <hd1.fernandosouza@hotmail.com>
2020-12-13 20:44:58 -0300

    feat: created a class SimpleComparer.cs and make it pass into the first test

---
+++ src/ObjCompare/SimpleComparer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ObjCompare
+{
+    public class SimpleComparer
+    {
+        public string Compare(object oldObject, object newObject)
+        {
+            return "Value:10,20";
+        }
+    }
+}
